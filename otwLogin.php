98cc40ef526a5ba75138f4a92fefe4ce7ba0f41f
deaaaww <natasyad610@gmail.com>
2026-05-04 14:45:01 +0700

    addddddd

--- otwLogin.php
+++ otwLogin.php
@@ -28,7 +28,7 @@ if (mysqli_num_rows($result) > 0) {
         $_SESSION['logged_in'] = true;
         $_SESSION['is_admin'] = false;
         echo "Login berhasil.";
-        header('Location: halo.php');
+        header('Location: index.php');
         exit();
     } 
     
@@ -37,3 +37,5 @@ if (mysqli_num_rows($result) > 0) {
     header('Location: index.php');
     exit();
 }
+
+?>
